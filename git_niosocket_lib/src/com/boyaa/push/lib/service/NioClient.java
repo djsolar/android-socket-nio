727e9e5478b8ef5ce5f05ec2c69a803cf682992f
Administrator <Administrator@DEEP-20111122HT>
2013-08-15 22:41:45 +0800

    优化

--- git_niosocket_lib/src/com/boyaa/push/lib/service/NioClient.java
+++ git_niosocket_lib/src/com/boyaa/push/lib/service/NioClient.java
@@ -11,6 +11,8 @@ import java.nio.channels.spi.SelectorProvider;
 import java.util.ArrayList;
 import java.util.Iterator;
 
+import com.boyaa.push.lib.util.LogUtil;
+
 import android.content.Context;
 import android.util.Log;
 
@@ -30,12 +32,11 @@ public class NioClient {
 	
 	private String IP="192.168.1.101";
 	private int PORT=60000;
-
-	private Selector selector;
-	private ByteBuffer readBuffer = ByteBuffer.allocate(8192);
-	
-	
 	private int state=STATE_CONNECT_START;
+	
+	Selector selector;
+	ByteBuffer readBuffer = ByteBuffer.allocate(8192);
+	SocketChannel socketChannel;
 	
 	private Thread conn=null;
 	private Thread rec=null;
@@ -44,7 +45,7 @@ public class NioClient {
 	private ISocketResponse respListener;
 	private ArrayList<Packet> requestQueen=new ArrayList<Packet>();
 	private final Object lock=new Object();
-	private final String TAG="Client";
+	private final String TAG="NioClient";
 	
 	public int send(Packet in)
 	{
@@ -146,19 +147,21 @@ public class NioClient {
 	{
 		public void run() {
 Log.v(TAG,"Conn :Start");
+
 			try {
 					state=STATE_CONNECT_START;
 					
-					InetSocketAddress address=new InetSocketAddress(IP, PORT);
 					selector=SelectorProvider.provider().openSelector();
-					SocketChannel socketChannel = SocketChannel.open();
+					socketChannel = SocketChannel.open();
 					socketChannel.configureBlocking(false);
+					
+					InetSocketAddress address=new InetSocketAddress(IP, PORT);
 					socketChannel.connect(address);
 					socketChannel.register(selector, SelectionKey.OP_CONNECT);
-					
+				
 					while(state!=STATE_CLOSE)
 					{
-						try {
+Log.v(TAG,"Conn :-------");
 								selector.select();
 								Iterator<SelectionKey> selectedKeys = selector.selectedKeys().iterator();
 								while (selectedKeys.hasNext())
@@ -193,60 +196,48 @@ Log.v(TAG,"Conn :Start");
 										key.interestOps(SelectionKey.OP_WRITE);
 									}
 								}
-								state=STATE_CONNECT_SUCCESS;
-						} catch (Exception e) {
-							e.printStackTrace();
-							state=STATE_CONNECT_FAILED;
-						}
 					}
+					
 			} catch (Exception e) {
 				e.printStackTrace();
+			}finally{
+				if(null!=socketChannel)
+				{
+					socketChannel.keyFor(selector).cancel();
+					try {
+						socketChannel.close();
+					} catch (IOException e1) {
+						e1.printStackTrace();
+					}
+				}
 			}
-
 Log.v(TAG,"Conn :End");
 		}
 		
 		
-		private void finishConnection(SelectionKey key) throws IOException {
+		private boolean finishConnection(SelectionKey key) throws IOException 
+		{
+			boolean result=false;
 			SocketChannel socketChannel = (SocketChannel) key.channel();
-		
-			// Finish the connection. If the connection operation failed
-			// this will raise an IOException.
-			try {
-				socketChannel.finishConnect();
-			} catch (IOException e) {
-				// Cancel the channel's registration with our selector
-Log.v("NioSocket", "finishConnection:"+e.getMessage());
-				key.cancel();
-				return;
+			result= socketChannel.finishConnect();//没有网络的时候也返回true
+			if(result)
+			{
+				key.interestOps(SelectionKey.OP_READ);
+				state=STATE_CONNECT_SUCCESS;
 			}
-		
-			// Register an interest in writing on this channel
-			key.interestOps(SelectionKey.OP_READ);
+Log.v(TAG,"finishConnection :"+result);
+			return result;
 		}
 		
 		
-		private void read(SelectionKey key) throws IOException {
+		private void read(SelectionKey key) throws IOException 
+		{
 			SocketChannel socketChannel = (SocketChannel) key.channel();
-
-			// Clear out our read buffer so it's ready for new data
 			readBuffer.clear();
-
-			// Attempt to read off the channel
 			int numRead;
-			try {
-				numRead = socketChannel.read(readBuffer);
-			} catch (IOException e) {
-				// The remote forcibly closed the connection, cancel
-				// the selection key and close the channel.
-				key.cancel();
-				socketChannel.close();
-				return;
-			}
-
-			if (numRead == -1) {
-				// Remote entity shut the socket down cleanly. Do the
-				// same from our end and cancel the channel.
+			numRead = socketChannel.read(readBuffer);
+			if (numRead == -1) 
+			{
 				key.channel().close();
 				key.cancel();
 				return;
@@ -257,7 +248,6 @@ respListener.onSocketResponse(new String(readBuffer.array(), 0, numRead));
 		private void write(SelectionKey key) throws IOException 
 		{
 			SocketChannel socketChannel = (SocketChannel) key.channel();
-			
 			synchronized (lock) 
 			{
 				 Packet item;
@@ -271,8 +261,6 @@ respListener.onSocketResponse(new String(readBuffer.array(), 0, numRead));
 				 }
 				 item=null;
 			}
-
-			
 			key.interestOps(SelectionKey.OP_READ);
 		}
 	}
